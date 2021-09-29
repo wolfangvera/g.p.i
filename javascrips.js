6446f321876ea6c9af4fe75e3cf83508f2333a60
Santiagoj <santitorresaguirre@gmail.com>
2021-09-28 22:31:50 -0500

    ensayo

---
+++ javascrips.js
@@ -0,0 +1,9 @@
+let Funcion = function(){
+    let numero = document.getElementsById("user").value;
+    return(
+        
+        alert("Los datos ingresados fueron= " + "\n"+
+        "----> "+ "Usuario= "+ numero +  "\n" +
+        "----> "+ "Contraseña= "+ numero)
+    );
+}
