726c880e405010e9eee059e11b875318687e8fd4
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-10-06 10:21:53 -0500

    Añadi el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../style/footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            Footer          
+        <footer className="footer">
+            <div className="row"> 
+
+
+            <ul className="info">
+                    <li  ><a href="#">Acerca de nosotros</a></li>
+                    <li  ><a href="#">Contactanos</a></li>
+                    <small>&copy; 2021</small>
+                </ul>
+
+
+                <div className="Redes">
+                    <h3>Follow us</h3>
+                    <ul className="siguenos">
+                        <li  ><a href="#"><i class="fab fa-facebook "></i></a></li>
+                        <li  ><a href="#"><i class="fab fa-github "></i></a></li>
+                        <li  ><a href="#"><i class="fab fa-linkedin"></i></a></li>
+
+                    </ul> 
+                </div>
+            </div>
+
         </footer>
     )
 }
