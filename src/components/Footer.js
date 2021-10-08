6480a91103c74b2ee4d2bfa712fea00460b7b56b
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-10-08 18:19:11 -0500

    Arreglo de iconos del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,9 +18,9 @@ const Footer = () => {
                 <div className="Redes">
                     <h3 className="Titulo_footer">Follow us</h3>
                     <ul className="siguenos">
-                        <li  ><a className="Enlaces" href="https://www.facebook.com"><i class="fab fa-facebook "></i></a></li>
-                        <li  ><a className="Enlaces" to="https://github.com/Santiago9j/ProyectoMinTIC"><i class="fab fa-github "></i></a></li>
-                        <li  ><a className="Enlaces" to="https://www.linkedin.com"><i class="fab fa-linkedin"></i></a></li>
+                        <li  ><a className="Enlaces" href="https://www.facebook.com"><i class="icono_footer fab fa-facebook "></i></a></li>
+                        <li  ><a className="Enlaces" to="https://github.com/Santiago9j/ProyectoMinTIC"><i class="icono_footer  fab fa-github "></i></a></li>
+                        <li  ><a className="Enlaces" to="https://www.linkedin.com"><i class="icono_footer fab fa-linkedin"></i></a></li>
 
                     </ul> 
                 </div>
