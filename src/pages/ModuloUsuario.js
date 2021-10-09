591ff2ceff4e2368981d6e5e360c971985a8342b
dmorenro <diegoxfy@gmail.com>
2021-10-08 19:36:18 -0500

    se ajsuta modulo usaurio para react

--- src/pages/ModuloUsuario.js
+++ src/pages/ModuloUsuario.js
@@ -1,8 +1,172 @@
 import React from 'react'
+import '../style/ModuloUsuario.css'
 
 const ModuloUsuario = () => {
     return (
         <div>
+            <main className="mainMU">        
+                <h2 className="h2MU">Módulo Gestión de Usuarios</h2>
+                <div className="contenedorMU">
+                     <table className="tableMU" border="1" align="center">
+                        <tr className="trMU">
+                            <th className="thMU">Identificador</th>
+                            <th className="thMU">Nombre</th>
+                            <th className="thMU">Estado</th>
+                            <th className="thMU">Rol</th>
+                            <th className="thMU"> Editar/ Guardar</th>
+                        </tr>
+                        <tr className="trMU">
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                        <tr className="trMU">
+                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                        <tr className="trMU">
+                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                        <tr className="trMU">
+                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                        <tr className="trMU">
+                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                        <tr className="trMU">
+                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
+                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
+                            <td className="tdMU">
+                                <select className="selectMU">
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Pendiente</option>
+                                    <option>Autorizado</option>
+                                    <option>No autorizado</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                <select className="selectMU"> 
+                                    <option selected disabled="">Selecciona una opción</option>
+                                    <option>Administrador</option>
+                                    <option>Vendedor</option>
+        
+                                </select>
+                            </td>
+                            <td className="tdMU">
+                                    <input className="inputMU" type="button" value="Editar" />
+                            </td>
+                        </tr>
+                    
+                    </table>
+                    <div>
+        
+                        <button className="botonModuloUsuarioMU" type="button" >Guardar cambios</button>
+                    </div>
+                </div>
+            </main>
             
         </div>
     )
