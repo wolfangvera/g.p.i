c86e2140ed3305750b578aa7d838866a2c469ae7
Santiago <santitorresaguirre@gmail.com>
2021-10-12 20:49:24 -0500

    Ensayo de axios

---
+++ src/PersonList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/api/product`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
