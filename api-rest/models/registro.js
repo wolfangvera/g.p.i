e0d117c660ba1553434a860d1fef82d120bad924
dmorenro <diegoxfy@gmail.com>
2021-10-23 17:02:33 -0500

    funcionaldiad Editar y eliminar en usuarios

--- api-rest/models/registro.js
+++ api-rest/models/registro.js
@@ -1,11 +1,14 @@
 'use strict'
 const mongoose = require("mongoose")
+const { text } = require('body-parser')
 const Schema = mongoose.Schema
 
 const RegistroSchema = Schema({
+    identificador: String,
     nombre: String,
     usuario: String,
     estado: {type: String, default: "Pendiente", enum:['Pendiente', 'Autorizado', 'No autorizado']},
+    rol: {type: String, default: "", enum:['Administrador', 'Vendedor']},
     password: String
 })
 
--- src/pages/ModuloUsuario.js
+++ src/pages/ModuloUsuario.js
@@ -1,175 +1,218 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
 import '../style/ModuloUsuario.css'
 
-const ModuloUsuario = () => {
+
+const Usuario = () => {
+
+    const [usuario, setUsuario] = useState([]);
+    const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
+    
+    
+
+    const obtenerUsuarios = async () => {
+        const options = { method: 'GET', url: 'http://localhost:3001/api/product' };
+        await axios
+            .request(options)
+            .then(function (response) {
+                setUsuario(response.data.products);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+        setEjecutarConsulta(false);    
+    };
+
+    useEffect(() => {
+        console.log('consulta', ejecutarConsulta);
+
+        if (ejecutarConsulta) {
+            obtenerUsuarios();
+        }
+    }, [ejecutarConsulta])
+
+    return (
+        <TablaUsuarios
+            listaUsuarios = {usuario}
+            setEjecutarConsulta = {setEjecutarConsulta}           
+        />
+        
+        )
+
+}
+
+
+const TablaUsuarios =({listaUsuarios, setEjecutarConsulta}) => {
+    useEffect(() => {
+        console.log("este es el estado de usuarios en el componente", listaUsuarios)
+
+    }, [listaUsuarios])
+
+
     return (
         <div>
-            <main className="mainMU">        
+                  
                 <h2 className="h2MU">Módulo Gestión de Usuarios</h2>
                 <div className="contenedorMU">
                      <table className="tableMU" border="1" align="center">
-                        <tr className="trMU">
+                        <thead className="trMU">
                             <th className="thMU">Identificador</th>
                             <th className="thMU">Nombre</th>
                             <th className="thMU">Estado</th>
                             <th className="thMU">Rol</th>
-                            <th className="thMU"> Editar/ Guardar</th>
-                        </tr>
-                        <tr className="trMU">
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU"> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                        <tr className="trMU">
-                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                        <tr className="trMU">
-                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                        <tr className="trMU">
-                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                        <tr className="trMU">
-                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                        <tr className="trMU">
-                        <td className="tdMU"> <input className="InputMU" placeholder="Ej:01658" type="text"/> </td>
-                            <td className="tdMU"> <input className="InputMU" placeholder="Ej:Pepito Perez" type="text"/> </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required>
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Pendiente</option>
-                                    <option>Autorizado</option>
-                                    <option>No autorizado</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                <select className="selectMU" defaultValue="" required> 
-                                    <option disabled value="">Selecciona una opción</option>
-                                    <option>Administrador</option>
-                                    <option>Vendedor</option>
-        
-                                </select>
-                            </td>
-                            <td className="tdMU">
-                                    <input className="input_edit" type="button" value="Editar" />
-                            </td>
-                        </tr>
-                    
+                            <th className="thMU"> Editar/ Eliminar</th>
+                        </thead>
+
+                        <tbody>                            
+                        {listaUsuarios.map((usuarios) => {
+                                return (
+                                    <FilaUsuario
+                                        key={nanoid()}
+                                        usuarios={usuarios}
+                                        setEjecutarConsulta={setEjecutarConsulta}
+                                        listaUsuarios ={listaUsuarios}
+
+                                    />
+                                )
+                            })
+                        }   
+                       
+                        </tbody>                                            
                     </table>
-                    <div className="bt_centrado">
-        
-                        <button className="botonModuloUsuarioMU" type="button" >Guardar cambios</button>
-                    </div>
                 </div>
-            </main>
+            
             
         </div>
     )
 }
 
-export default ModuloUsuario
+
+const FilaUsuario = ({ usuario, setEjecutarConsulta }) => {
+    const [edit, setEdit] = useState(false);
+    const [infoNuevoUsuario, setInfoNuevoUsuario] = useState({
+        nombre: usuario.nombre,
+        usuario: usuario.usuario,
+        estado: usuario.estado,
+        rolUsuario: usuario.rol,
+    });
+
+    const actualizarUsuario = async () => {
+        console.log(infoNuevoUsuario)
+
+        const options = {
+            method: 'PUT',
+            url: `http://localhost:3001/api/product/${usuario._id}`,
+            headers: { 'Content-Type': 'application/json' },
+            data: { ...infoNuevoUsuario, productId: usuario._id}
+        }
+
+        await axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
+            setEdit(false);
+            setEjecutarConsulta(true);
+            //toast.success("usuario modificado con exito");
+
+        }).catch(function (error) {
+            console.error(error);
+            //toast.error("Error modificando el usuario")            
+        });
+    }
+    
+
+
+    const eliminarUsuario = async () => {
+        console.log(infoNuevoUsuario)
+
+        const options = {
+            method: 'PUT',
+            url: `http://localhost:3001/api/product/${usuario._id}`,
+            headers: { 'Content-Type': 'application/json' },
+            data: { ...infoNuevoUsuario, productId: usuario._id}
+        }
+
+        await axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
+            setEdit(false);
+            setEjecutarConsulta(true);
+            //toast.success("usuario modificado con exito");
+
+        }).catch(function (error) {
+            console.error(error);
+            //toast.error("Error modificando el usuario")            
+        });
+
+    }
+    
+    
+    return(
+        <tr>
+            {edit ? (
+                <>
+                    <td className="tdMU"> {usuario.identificador}</td>
+                    <td className="tdMU"> {usuario.nombre} </td>
+                    <td className="tdMU">
+                        <select className="selectMU"
+                            value ={infoNuevoUsuario.estadoUsuario}
+                            onChange={ (e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, estadoUsuario: e.target.value})}
+                            >
+                            <option disabled value="">Selecciona una opción</option>
+                            <option>Pendiente</option>
+                            <option>Autorizado</option>
+                            <option>No autorizado</option>        
+                        </select>
+                    </td>
+                    <td className="tdMU">
+                        <select className="selectMU"
+                            value ={infoNuevoUsuario.rolUsuario}
+                            onChange={ (e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, rolUsuario: e.target.value})}
+                            > 
+                            <option disabled value="">Selecciona una opción</option>
+                            <option>Administrador</option>
+                            <option>Vendedor</option>        
+                        </select>
+                    </td>       
+                </>
+            ):(
+                <>
+                <td className="tdMU">{usuario.identificador}</td>
+                <td className="tdMU">{usuario.nombre} </td>
+                <td className="tdMU">{usuario.rol} </td>
+                <td className="tdMU"> {usuario.estado} </td>                               
+                </>
+            )}
+
+            <td className="tdMU">
+                <div className="flex w-full justify-around">
+                    {edit ? (
+                        <i
+                            onClick={() => actualizarUsuario()}
+                            className="fas fa-check text-green-700 hover:text-green-500"
+                        />
+
+
+                    ) : (
+                        <i
+                            onClick={() => setEdit(!edit)}
+                            className='fas fa-pencil-alt text-yellow-700 hover:text-yellow-500'
+                        />
+                    )}
+
+                    <i
+                        onClick={() => eliminarUsuario()}
+                        className="fas fa-trash text-red-700 hover:text-yellow-500" />
+                </div>
+            </td>
+        </tr>
+        
+        )
+    
+} 
+   
+
+export default Usuario
