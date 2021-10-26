24d06fc6eb44284ae91d18f9a2a21ba3c66fb510
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-10-26 08:46:36 -0500

    establecimiento de headers

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3001; // Step 1
 
 const routes = require('./routes/index');
 
+
 // Step 2
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.kxsmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -24,6 +25,14 @@ mongoose.connection.on('connected', () => {
 // Data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
 
 // Step 3
 
