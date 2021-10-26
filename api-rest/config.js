d40e257e7036b5baaf5e78085ee9b4b760ece2af
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-10-25 22:48:27 -0500

    modificar codigo para despliegue

--- api-rest/config.js
+++ api-rest/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || "mongodb+srv://admin:admin@cluster0.kxsmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    db: process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.kxsmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     SECRET_TOKEN: 'miclavedetokens'
 
     
