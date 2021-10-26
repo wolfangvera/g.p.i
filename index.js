62f18d1b3c36d81814abde48478e726e88b7168e
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-10-26 08:14:24 -0500

    w2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Import npm packages
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
+
 const path = require('path');
 
 const app = express();
